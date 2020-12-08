2d6253a8b9ffcf5c66f6eabcce2afa0d7bae63ad
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-12-08 12:26:32 -0500

    Initial commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Bullet Related")]
+    public int MaxBullets;
+    public BulletType enemyBulletType;
+    public BulletType playerBulletType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Kickoff the BulletManager
+        BulletManager.Instance().Init(MaxBullets, enemyBulletType, playerBulletType);
+    }
+
+}
